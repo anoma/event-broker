52e3a7d731fbfb43b25c08c60a528c2c3d6e6e91
mariari <mariari@protonmail.com>
2026-03-20 22:35:34 +0800

    Add jason for the jason deriving functionality

--- global_deps.exs
+++ global_deps.exs
@@ -1,5 +1,6 @@
 # keep me alphabetized; non-runtime dependencies at the bottom.
 [
+  {:jason, "~> 1.4"},
   {:typed_struct, "~> 0.3.0"},
   # non-runtime dependencies below
   {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
