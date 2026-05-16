7809b1ae48e0e1492da40c2f56c72133ce5c4f57
mariari <mariari@protonmail.com>
2026-05-16 21:16:40 +0800

    Make the project properly work on Hex

--- mix.exs
+++ mix.exs
@@ -1,15 +1,10 @@
 defmodule EventBroker.MixProject do
   use Mix.Project
 
-  def version do
-    {ver, _} = Code.eval_file("version.exs")
-    ver
-  end
-
   def project do
     [
       app: :event_broker,
-      version: version(),
+      version: "1.1.0",
       build_path: "_build",
       config_path: "config/config.exs",
       package: package(),
@@ -42,14 +37,13 @@ defmodule EventBroker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
-    ] ++ global_deps()
-  end
-
-  def global_deps do
-    {list, _} = Code.eval_file("global_deps.exs")
-    list
+      {:ex_example, "~> 0.1.1"},
+      {:jason, "~> 1.4"},
+      {:typed_struct, "~> 0.3.0"},
+      # non-runtime dependencies below
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.31", only: [:dev], runtime: false}
+    ]
   end
 end
